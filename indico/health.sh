3f84f42690a23846a4bcd6ed5bad3e5cc069345f
Marco Vidal Garcia <marco.vidal@cern.ch>
2018-01-24 17:25:24 +0100

    Update health script to use cURL

--- indico/health.sh
+++ indico/health.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ps aux | pgrep uwsgi
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://test-mvg.web.cern.ch)
+
+if [ $STATUS_CODE -eq 200 ]; then
+    exit 0
+else
+    exit 1
+fi
