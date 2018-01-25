d9dc726fe0a05cc12d9a7160fb3bf912442cda34
Marco Vidal Garcia <marco.vidal@cern.ch>
2018-01-25 16:50:59 +0100

    Use self uwsgi-tools

--- indico/health.sh
+++ indico/health.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-STATUS_LINE=$(./opt/indico/.venv/bin/uwsgi_curl 127.0.0.1:59999 | grep HTTP/1.1)
-STATUS_LINE_ARRAY=($STATUS_LINE)
-STATUS_CODE=${STATUS_LINE_ARRAY[1]}
-
+STATUS_CODE=$(./opt/indico/.venv/bin/uwsgi_curl -s 127.0.0.1:59999)
 if [ $STATUS_CODE -eq 200 ]; then
     exit 0
 else
