43cb5469944327125714f6ee90a9272ce5764eab
Marco Vidal Garcia <marco.vidal@cern.ch>
2018-01-25 13:46:02 +0100

    Intall uwsgi-tools & modify health script

--- indico/health.sh
+++ indico/health.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://test-mvg.web.cern.ch)
+STATUS_LINE=$(./opt/indico/.venv/bin/uwsgi_curl 127.0.0.1:59999 | grep HTTP/1.1)
+STATUS_LINE_ARRAY=($STATUS_LINE)
+STATUS_CODE=${STATUS_LINE_ARRAY[1]}
 
 if [ $STATUS_CODE -eq 200 ]; then
     exit 0
